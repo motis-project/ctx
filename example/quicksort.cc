3ce4268cadd21121d1109f5f9b851811c6afce58
Felix Gündling <felixguendling@gmail.com>
2016-03-02 23:59:37 +0100

    wip

--- example/quicksort.cc
+++ example/quicksort.cc
@@ -1,5 +1,25 @@
 #include <cstdio>
+#include <vector>
+#include <algorithm>
+
+template <class ForwardIt>
+void quicksort(ForwardIt first, ForwardIt last) {
+  if (first == last) {
+    return;
+  }
+
+  auto pivot = *std::next(first, std::distance(first, last) / 2);
+  auto mid1 = std::partition(first, last,
+                             [pivot](const auto& em) { return em < pivot; });
+  auto mid2 = std::partition(mid1, last,
+                             [pivot](const auto& em) { return !(pivot < em); });
+
+  quicksort(first, mid1);
+  quicksort(mid2, last);
+}
 
 int main() {
-  printf("not implemented!\n");
+  std::vector<int> v = {3, 1, 2};
+  quicksort(begin(v), end(v));
+  printf("%s\n", std::is_sorted(begin(v), end(v)) ? "works" : "does not work");
 }
