43138981240f5afafe46fcc7d8eefc77d6e1e155
Felix Gündling <guendling@algo.informatik.tu-darmstadt.de>
2018-04-13 15:47:56 +0200

    locking example working

--- example/locking.cc
+++ example/locking.cc
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <iostream>
 #include <mutex>
 #include <thread>
 #include <vector>
@@ -20,16 +21,22 @@ struct controller {
     controller& ctrl_;
   };
 
+  controller(boost::asio::io_service& ios) : scheduler_{ios} {};
+
   void start_read() {
-    std::unique_lock<std::mutex> l{lock_};
+    std::unique_lock<std::mutex> l{lock_, std::defer_lock_t{}};
+
+  aquire:
+    l.lock();
     if (write_active_) {
       auto& op = current_op<controller*>();
       read_queue_.emplace_back(op.shared_from_this());
       l.unlock();
       op.suspend(/*finish = */ false);
-    } else {
-      ++read_count_;
+      goto aquire;
     }
+
+    ++read_count_;
   }
 
   void end_read() {
@@ -43,15 +50,19 @@ struct controller {
   }
 
   void start_write() {
-    std::unique_lock<std::mutex> l{lock_};
+    std::unique_lock<std::mutex> l{lock_, std::defer_lock_t{}};
+
+  aquire:
+    l.lock();
     if (write_active_ || read_count_ != 0) {
       auto& op = current_op<controller*>();
       read_queue_.emplace_back(op.shared_from_this());
       l.unlock();
       op.suspend(/*finish = */ false);
-    } else {
-      write_active_ = true;
+      goto aquire;
     }
+
+    write_active_ = true;
   }
 
   void end_write() {
@@ -73,11 +84,22 @@ struct controller {
 
   template <typename Fn>
   void enqueue_read(Fn&& fn, op_id id) {
-    enqueue(this,
-            []() {
+    scheduler_.enqueue(this,
+                       [fn, this]() {
+                         read r{*this};
+                         fn();
+                       },
+                       id);
+  }
 
-            },
-            id);
+  template <typename Fn>
+  void enqueue_write(Fn&& fn, op_id id) {
+    scheduler_.enqueue(this,
+                       [fn, this]() {
+                         write r{*this};
+                         fn();
+                       },
+                       id);
   }
 
   scheduler<controller*> scheduler_;
@@ -89,41 +111,44 @@ struct controller {
 };
 
 int main() {
-  std::vector<int> data;
+  std::vector<uint64_t> data({1, 1});
 
-  auto const read_op = [&data]() {
-    int sum = 0;
+  std::mutex m;
+
+  auto const read_op = [&data, &m]() {
+    uint64_t sum = 0;
     for (auto const& d : data) {
       sum += d;
     }
-    printf("%d\n", sum);
+    if (sum != 0 && sum % 1000 == 0) {
+      std::lock_guard<std::mutex> l{m};
+      std::cout << sum << "\n";
+    }
   };
 
   auto const write_op = [&data]() {
-    int sum = 0;
-    for (auto const& d : data) {
-      sum += d;
-    }
-    data.emplace_back(sum);
+    data.emplace_back(data[data.size() - 1] + data[data.size() - 2]);
   };
 
   boost::asio::io_service ios;
   controller c(ios);
-  for (int i = 0; i < 10000; ++i) {
-    c.enqueue(read_op, op_id("read", "?", 0));
-    c.enqueue(read_op, op_id("read", "?", 0));
-    c.enqueue(read_op, op_id("read", "?", 0));
-    c.enqueue(read_op, op_id("read", "?", 0));
-    c.enqueue(read_op, op_id("read", "?", 0));
-    c.enqueue(read_op, op_id("read", "?", 0));
-    c.enqueue(read_op, op_id("read", "?", 0));
-    c.enqueue(write_op, op_id("write", "?", 0));
+  for (int i = 0; i < 2000; ++i) {
+    c.enqueue_read(read_op, op_id("read", "?", 0));
+    c.enqueue_read(read_op, op_id("read", "?", 0));
+    c.enqueue_read(read_op, op_id("read", "?", 0));
+    c.enqueue_read(read_op, op_id("read", "?", 0));
+    c.enqueue_read(read_op, op_id("read", "?", 0));
+    c.enqueue_read(read_op, op_id("read", "?", 0));
+    c.enqueue_read(read_op, op_id("read", "?", 0));
+    c.enqueue_write(write_op, op_id("write", "?", 0));
   }
 
-  constexpr auto const worker_count = 8;
+  constexpr auto const worker_count = 4;
   std::vector<std::thread> threads(worker_count);
   for (int i = 0; i < worker_count; ++i) {
     threads[i] = std::thread([&]() { ios.run(); });
   }
   std::for_each(begin(threads), end(threads), [](std::thread& t) { t.join(); });
+
+  std::cout << data.back() << "\n";
 }
